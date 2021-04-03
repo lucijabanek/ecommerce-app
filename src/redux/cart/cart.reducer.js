cd51e1ca4a70ad9e0a870e32d0d3ad79bd50ca81
Lucija Banek <lbanek00@fesb.hr>
2021-04-03 22:00:20 +0200

    Updated cart utils to group items in cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionTypes from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
+  cardItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cardItems: addItemToCart(state.cardItems, action.payload),
       };
     default:
       return state;
