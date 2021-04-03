43c9df4039d411a66b1a1f028135d164f8517e9c
Lucija Banek <lbanek00@fesb.hr>
2021-04-04 01:22:26 +0200

    Created cart-item component and connected to cart dropdown with cardItems reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 import SHOP_DATA from "./shop.data.js";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-import "./shop.styles.scss";
-
 class ShopPage extends Component {
   constructor(props) {
     super(props);
