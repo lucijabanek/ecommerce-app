4a38a9f45cf7979b8b962a2a1e2fc2897f216f6e
Lucija Banek <lbanek00@fesb.hr>
2021-04-09 00:47:23 +0200

    Converted all components over to styled-components

--- src/components/form-input/form-input.components.jsx
+++ src/components/form-input/form-input.components.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-import "./form-input.styles.scss";
+import {
+  GroupContainer,
+  FormInputContainer,
+  FormInputLabel,
+} from "./form-input.styles";
 
 //we need handleChange because we want to bubble up any on change that
 //the input has
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="group">
-    <input className="form-input" onChange={handleChange} {...otherProps} />
+  <GroupContainer>
+    <FormInputContainer onChange={handleChange} {...otherProps} />
     {label ? (
-      <label
-        className={`${otherProps.value.length ? "shrink" : ""}form-input-label`}
-      >
+      <FormInputLabel className={`${otherProps.value.length ? "shrink" : ""}`}>
         {label}
-      </label>
+      </FormInputLabel>
     ) : null}
-  </div>
+  </GroupContainer>
 );
 export default FormInput;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import Directory from "../../components/directory/directory.component";
 
-import "./homepage.styles.scss";
+import { HomePageContainer } from "./homepage.styles";
 
 //it will be functional component because we don't really need any
 //lifecycle method at this point or store any state
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
