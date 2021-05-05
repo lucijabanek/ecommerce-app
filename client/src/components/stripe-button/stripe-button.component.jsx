22b371e18f1ecea41461b742f63b3fb9f64b389f
Lucija Banek <lbanek00@fesb.hr>
2021-05-05 20:30:59 +0200

    Separating app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51IcfLcFucsSgDdOesFSTfmG8IagQhuSzz7Cv5MeA0fpSmhxdqz8Ha7vM5igvkjGFBPxVMQ6uM8UhkPhPdmr3aHeZ00hDaALLXy";
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please sure you use the provided credit card."
+        );
+      });
   };
   return (
     <StripeCheckout
@@ -15,6 +31,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="eCommerce App"
       billingAddress
       shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
